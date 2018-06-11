349c15ae3fb46600d562ad803f4f61a5a3bdd8f7
Kalyan <kalyan.annamalai@transport.nsw.gov.au>
2018-06-12 04:11:38 +1000

    use async pipe

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {PokeApiService} from '../../';
 import {Pokemon} from '../../';
-import { finalize, map, catchError } from 'rxjs/operators';
-import {Subscription, throwError} from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'pokemon-gallery',
@@ -10,52 +10,42 @@ import {Subscription, throwError} from 'rxjs';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  pokemons: Pokemon[];
+  pokemon$: Observable<Pokemon[]>
   pageSize = 20;
-  isLoadingData = true;
   currentPage = 1;
-  searchText = '';
-  pokeApiSubscription: Subscription;
   networkError = false;
 
   constructor(private pokeApiService: PokeApiService) {
   }
 
   ngOnInit() {
-    this.refreshPokemons();
+    this.refreshPokemons('');
   }
 
-  private refreshPokemons() {
-    if (this.pokeApiSubscription) {
-      this.pokeApiSubscription.unsubscribe();
-    }
-    this.isLoadingData = true;
+  private refreshPokemons(searchText?: string) {
     this.currentPage = 1;
     this.networkError = false;
-    this.pokeApiSubscription = this.pokeApiService.search(this.searchText)
-                                            .pipe(
-                                              map(pokemons => this.sortByNumber(pokemons)),
-                                              finalize(() => this.isLoadingData = false),
-                                              catchError(err => {
-                                                this.networkError = true;
-                                                return throwError(err);
-                                              })
-                                            )
-                                            .subscribe(pokemons => this.pokemons = pokemons);
+    this.pokemon$ = this.pokeApiService.search(searchText)
+      .pipe(
+        map(pokemons => this.sortByNumber(pokemons)),
+        catchError(err => {
+          this.networkError = true;
+          return of([]);
+        })
+      );
   }
 
   private sortByNumber(pokemons: Pokemon[]): Pokemon[] {
     return pokemons.sort((a: Pokemon, b: Pokemon) => a.index - b.index);
   }
 
-  get paginatedList(): Pokemon[] {
+  paginatedList(pokemons: Pokemon[]): Pokemon[] {
     const start = (this.currentPage * this.pageSize) - this.pageSize;
     const end = this.currentPage * this.pageSize;
-    return this.pokemons.slice(start, end);
+    return pokemons.slice(start, end);
   }
 
   onSearch(text: string) {
-    this.searchText = text;
-    this.refreshPokemons();
+    this.refreshPokemons(text);
   }
 }
